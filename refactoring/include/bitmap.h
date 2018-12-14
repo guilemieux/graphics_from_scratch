05af25fe464c389bf5a31dcbc822cf17a85803f2
guillaume lemieux <guilemieux@gmail.com>
2018-12-14 13:43:25 -0500

    Modify bitmap to allow change of dimention

--- refactoring/include/bitmap.h
+++ refactoring/include/bitmap.h
@@ -5,10 +5,11 @@
 
 typedef struct bitmap Bitmap_t;
 
-extern Bitmap_t *new_bitmap(int width, int height);
+extern Bitmap_t *new_bitmap();
 extern void free_bitmap(Bitmap_t *bitmap);
-extern int bitmap_width(Bitmap_t *bitmap);
-extern int bitmap_height(Bitmap_t *bitmap);
+extern void bitmap_set_dim(Bitmap_t *bitmap, int width, int height);
+extern int bitmap_get_width(Bitmap_t *bitmap);
+extern int bitmap_set_height(Bitmap_t *bitmap);
 extern struct color bitmap_get_color_at(Bitmap_t *bitmap, int row, int col);
 extern void bitmap_set_color_at(Bitmap_t *bitmap, struct color c, int row, int col);
 
